f3b85efb4662e94020fff784c8446d13d66a1cd1
dasdawidt <David_Maier@gmx.de>
2023-11-29 00:52:08 +0100

    feat: add router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
 import HomeView from '@/views/HomeView.vue';
+import ReaderView from '@/views/ReaderView.vue';
 
 export default createRouter({
     history: createWebHistory(),
@@ -9,6 +10,11 @@ export default createRouter({
             path: '/',
             name: 'home',
             component: HomeView,
+        },
+        {
+            path: '/reader',
+            name: 'reader',
+            component: ReaderView,
         },
     ],
 });
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,33 @@
 <script setup lang="ts">
+import BookDialogue from '@/components/inputs/BookDialogue.vue';
+import TranslationDialogue from '@/components/inputs/TranslationDialogue.vue';
+import VerseDialogue from '@/components/inputs/VerseDialogue.vue';
+import { translation } from '@/logic/StubTranslationProvider';
+import { Book } from '@/types/bible/book';
+import { Translation } from '@/types/bible/translation';
+import { TranslationList } from '@/types/bible/translationList';
+import { Passage } from '@/types/plans/passage';
+import { ref } from 'vue';
+
+const translationLost: TranslationList = [
+    {
+        id: 'de',
+        name: 'German',
+        translations: [
+            translation
+        ]
+    }
+];
+
+const selectedTranslation = ref<Translation>(undefined);
+const selectedBook = ref<Book>(undefined);
+const selectedPassages = ref<Passage[]>([]);
+
 </script>
 
 <template>
-    <h1>Hello World!</h1>
+    <h1>Select Verses</h1>
+    <TranslationDialogue v-model="selectedTranslation" :translations="translationLost" />
+    <BookDialogue v-model="selectedBook" :books="selectedTranslation?.books" />
+    <VerseDialogue v-model="selectedPassages" :translation="selectedTranslation" :book="selectedBook?.type" />
 </template>
