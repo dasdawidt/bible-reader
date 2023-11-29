5c7769422d26dfda29cf9dc7409364a807b05cf8
dasdawidt <David_Maier@gmx.de>
2023-11-29 17:13:18 +0100

    fix: make dialogue select button prettier

--- src/components/inputs/DialogueSelectButton.vue
+++ src/components/inputs/DialogueSelectButton.vue
@@ -1,19 +1,20 @@
 <script setup lang="ts">
-import Button, { ButtonProps } from 'primevue/button';
+import Dropdown, { DropdownProps } from 'primevue/dropdown';
 
 // The @vue-ignore is needed because vue is not able to resolve the base type at the moment.
-export interface Props extends /* @vue-ignore */ ButtonProps { };
+export interface Props extends /* @vue-ignore */ DropdownProps { };
 
 const props = defineProps<Props & { disabled?: boolean }>();
 
 </script>
 
 <template>
-    <Button v-bind="props" class="w-full p-inputtext cursor-pointer" :class="{ 'p-disabled': props.disabled }" unstyled>
-        <div class="w-full flex flex-row justify-start alignt-center">
-            <slot></slot>
-            <div class="flex-grow" />
-            <i class="mdi mdi-chevron-right leading-none text-xl opacity-75" />
-        </div>
-    </Button>
+    <Dropdown v-bind="props" readonly class="w-full" :class="{ 'p-disabled': props.disabled }"
+        dropdown-icon="mdi mdi-chevron-right text-xl" empty-message=" " :pt="{ panel: { class: 'hidden' } }">
+        <template #value>
+            <div class="w-full flex flex-row justify-start alignt-center">
+                <slot></slot>
+            </div>
+        </template>
+    </Dropdown>
 </template>
