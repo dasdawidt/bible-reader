0e852db063f92dabc3c352fda1413e02ad330caf
dasdawidt <David_Maier@gmx.de>
2023-12-01 10:45:52 +0100

    fix: remove stub translation

--- src/logic/translations/provider.ts
+++ src/logic/translations/provider.ts
@@ -1,11 +1,10 @@
 import { TranslationList } from '@/types/bible/translationList';
-import { stubTranslation } from './stubTranslation';
 import { gloTranslation } from './gloTranslation';
 
 export const supportedTranslations: TranslationList = [
     {
         id: 'de',
         name: 'German',
-        translations: [stubTranslation, gloTranslation],
+        translations: [gloTranslation],
     },
 ];
