3628e42063614a712d58e0e210687e01d56cdba9
dasdawidt <David_Maier@gmx.de>
2023-12-07 22:17:25 +0100

    feat: extend translation provider by supported chapters and -verses

--- src/types/logic/TranslationProvider.d.ts
+++ src/types/logic/TranslationProvider.d.ts
@@ -4,11 +4,14 @@ import { BookType } from '../bible/bookType';
 import { Book } from '../bible/book';
 import { Chapter } from '../bible/chapter';
 import { Verse } from '../bible/verse';
+import { ChapterInfo } from '../bible/chapterInfo';
 
 export type TranslationProvider = {
     info: TranslationInfo;
     supportedBooks: BookInfo[];
     getBook(type: BookType): Promise<Book>;
+    supportedChapters(type: BookType): Promise<ChapterInfo[]>;
     getChapter(book: BookType, chapter: number): Promise<Chapter>;
+    supportedVerses(book: BookType, chapter: number): Promise<Verse[]>;
     getVerse(book: BookType, chapter: number, verse: number): Promise<Verse>;
 };
