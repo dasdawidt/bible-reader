b15762e0b2ebe3735fde9c106543813a573c01a2
dasdawidt <David_Maier@gmx.de>
2023-12-08 01:28:32 +0100

    feat: grouped books by testament

--- src/components/inputs/BookDialogue.vue
+++ src/components/inputs/BookDialogue.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
 import Dialog from "primevue/dialog";
 import Button from "primevue/button";
 import Listbox from "primevue/listbox";
+import Divider from "primevue/divider";
+import DialogueSelectButton from "./DialogueSelectButton.vue";
+import { ref, computed } from "vue";
 import { Book } from "@/types/bible/book";
 import { useOnMobile } from "@/logic/util/MobileDetection";
-import DialogueSelectButton from "./DialogueSelectButton.vue";
-// import { BookTypeOldTestament } from "@/types/bible/bookTypeOldTestament";
-// import { BookTypeNewTestament } from "@/types/bible/bookTypeNewTestament";
+import { BookTypeOldTestament } from "@/types/bible/bookTypeOldTestament";
 
 const props = defineProps<{
     /**
@@ -20,19 +20,6 @@ const props = defineProps<{
     modelValue?: Book
 }>();
 
-// const bookList = computed(() => [
-//     {
-//         name: 'Old Testament',
-//         type: BookTypeOldTestament,
-//         books: props.books.filter(b => Object.values<number>(BookTypeOldTestament).),
-//     },
-//     {
-//         name: 'New Testament',
-//         type: BookTypeNewTestament,
-//         books: Book[],
-//     }
-// ])
-
 const emits = defineEmits<{
     (event: 'update:modelValue', value: Book): void
 }>();
@@ -41,6 +28,16 @@ const selectedBook = computed({
     get: () => props.modelValue,
     set: v => emits('update:modelValue', v)
 });
+
+const groupedBooks = computed(() => props.books.reduce<[{ name: string, books: Book[] }, { name: string, books: Book[] }]>(
+    (obj, b) => {
+        Object.keys(BookTypeOldTestament).includes(b.type.toString())
+            ? obj[0].books = [...obj[0].books, b]
+            : obj[1].books = [...obj[1].books, b];
+        return obj;
+    },
+    [{ name: 'Old Testament', books: [] }, { name: 'New Testament', books: [] }]
+));
 
 const { isOnMobile } = useOnMobile();
 const visible = ref(false);
@@ -60,13 +57,19 @@ const disabled = computed(() => props.books == null);
     <Dialog v-model:visible="visible" :closable="false" :draggable="false" modal header="Select Book"
         :position="isOnMobile ? 'bottom' : 'top'" dismissableMask class="w-full max-w-container"
         :class="[!isOnMobile || 'max-h-bottom-sheet']">
-        <Listbox v-model="selectedBook" :options="books" optionLabel="name" class="w-full" @change="visible = false"
-            filterPlaceholder="Filter...">
+        <Listbox v-model="selectedBook" :options="groupedBooks" optionLabel="name" option-group-children="books"
+            option-group-label="name" class="w-full" @change="visible = false" filterPlaceholder="Filter..."
+            :pt="{ itemGroup: { class: 'bg-transparent' } }">
             <template #option="slotProps">
                 <div class="flex align-items-center gap-2">
                     <div class="w-12 flex-shrink-0 opacity-50 overflow-hidden text-ellipsis">{{
                         slotProps.option.abbreviation?.toUpperCase() }}</div>
                     <div>{{ slotProps.option.name }}</div>
+                </div>
+            </template>
+            <template #optiongroup="slotProps">
+                <div class="flex flex-col gap-3">
+                    <span>{{ slotProps.option.name }}</span>
                 </div>
             </template>
         </Listbox>
