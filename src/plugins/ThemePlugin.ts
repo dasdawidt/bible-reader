dfcffcd308a28a33467d00f5f8654dc1b093dc93
dasdawidt <David_Maier@gmx.de>
2023-12-18 00:55:06 +0100

    feat: added dynamic theme color value

--- src/plugins/ThemePlugin.ts
+++ src/plugins/ThemePlugin.ts
@@ -6,7 +6,10 @@ import {
 import { Plugin, computed, ref, watch } from 'vue';
 import lightTheme from '@/assets/themes/light.css?inline';
 import darkTheme from '@/assets/themes/dark.css?inline';
+import { useOnMobile } from '@/logic/util/MobileDetection';
 
+const { isOnMobile } = useOnMobile();
+const themeColorMetaTag = ref<HTMLMetaElement>(undefined);
 const prefersColorScheme = usePreferredColorScheme();
 const colorSchemeOverride = ref<ColorSchemeType>(undefined);
 const colorScheme = computed(() =>
@@ -31,14 +34,25 @@ watch(colorScheme, (newValue, _oldValue) => {
     if (newValue === 'dark') {
         unloadLightTheme();
         loadDarkTheme();
+        setThemeColor(isOnMobile.value ? '#090d15' : '#111827');
     } else {
         unloadDarkTheme();
         loadLightTheme();
+        setThemeColor(isOnMobile.value ? '#ffffff' : '#f9fafb');
     }
 });
 
+function setThemeColor(color: string) {
+    themeColorMetaTag.value.content = color;
+}
+
 const ThemePlugin: Plugin = {
     install(_app, ..._options) {
+        const element = document.createElement('meta');
+        element.name = 'theme-color';
+        element.content = '#f9fafb';
+        document.head.appendChild(element);
+        themeColorMetaTag.value = element;
         colorSchemeOverride.value = 'no-preference';
     },
 };
