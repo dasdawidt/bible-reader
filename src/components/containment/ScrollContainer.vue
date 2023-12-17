6cc925e01192d0832be0ac8845bd1d4d7b4d6d94
dasdawidt <David_Maier@gmx.de>
2023-12-17 23:01:54 +0100

    feat: created scroll container component

---
+++ src/components/containment/ScrollContainer.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import ScrollPanel from 'primevue/scrollpanel';
+</script>
+
+<template>
+    <ScrollPanel
+        :pt="{ content: { class: 'box-border' }, barY: { class: 'transition-opacity bg-slate-500 opacity-20 w-2 hover:opacity-50 active:opacity-50 cursor-default' } }">
+        <slot />
+    </ScrollPanel>
+</template>
