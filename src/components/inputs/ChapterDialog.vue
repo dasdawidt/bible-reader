948a8b3f9e7dd99f0bcea644f0839e0571942f25
dasdawidt <David_Maier@gmx.de>
2023-12-17 23:02:17 +0100

    feat: use scroll container component

--- src/components/inputs/ChapterDialog.vue
+++ src/components/inputs/ChapterDialog.vue
@@ -4,8 +4,9 @@ import Dialog from "primevue/dialog";
 import Button from "primevue/button";
 import Listbox from "primevue/listbox";
 import { useOnMobile } from "@/logic/util/MobileDetection";
-import DialogueSelectButton from "./DialogSelectButton.vue";
+import DialogSelectButton from "./DialogSelectButton.vue";
 import { Chapter } from "@/types/bible/chapter";
+import ScrollContainer from "../containment/ScrollContainer.vue";
 
 const props = defineProps<{
     /**
@@ -37,25 +38,27 @@ const disabled = computed(() => props.chapters == null);
 </script>
 
 <template>
-    <DialogueSelectButton @click="visible = true" :disabled="disabled">
+    <DialogSelectButton @click="visible = true" :disabled="disabled">
         <div v-if="selectedChapter && !disabled" class="flex align-items-center">
             <div>{{ bookName ?? 'Chapter' }} {{ selectedChapter?.number }}</div>
         </div>
         <div v-else>
             Select Chapter...
         </div>
-    </DialogueSelectButton>
+    </DialogSelectButton>
     <Dialog v-model:visible="visible" :closable="false" :draggable="false" modal header="Select Chapter"
         :position="isOnMobile ? 'bottom' : 'top'" dismissableMask class="w-full max-w-container"
-        :class="[!isOnMobile || 'max-h-bottom-sheet']">
-        <Listbox v-model="selectedChapter" :options="chapters" class="w-full" @change="visible = false"
-            filterPlaceholder="Filter...">
-            <template #option="slotProps">
-                <div class="flex align-items-center">
-                    <div>{{ bookName ?? 'Chapter' }} {{ slotProps.option.number }}</div>
-                </div>
-            </template>
-        </Listbox>
+        :pt="{ content: { class: 'overflow-hidden' } }">
+        <ScrollContainer class="max-h-bottom-sheet">
+            <Listbox v-model="selectedChapter" :options="chapters" class="w-full" @change="visible = false"
+                filterPlaceholder="Filter...">
+                <template #option="slotProps">
+                    <div class="flex align-items-center">
+                        <div>{{ bookName ?? 'Chapter' }} {{ slotProps.option.number }}</div>
+                    </div>
+                </template>
+            </Listbox>
+        </ScrollContainer>
         <template #footer>
             <Button label="Cancel" @click="visible = false" severity="secondary" text class="w-full" />
         </template>
