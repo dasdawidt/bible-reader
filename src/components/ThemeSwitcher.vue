ba4b5a8795523f20ab7acb03eb5a515f3ab1f54f
dasdawidt <David_Maier@gmx.de>
2023-12-17 03:33:06 +0100

    feat: add icons to theme swicther

--- src/components/ThemeSwitcher.vue
+++ src/components/ThemeSwitcher.vue
@@ -2,28 +2,42 @@
 import SelectButton from 'primevue/selectbutton';
 import { useTheme } from '@/plugins/ThemePlugin';
 import { ColorSchemeType } from '@vueuse/core';
+import { useOnMobile } from '@/logic/util/MobileDetection';
 
+const { isOnMobile } = useOnMobile();
 const colorScheme = useTheme();
 const options: {
     label: string,
-    value: ColorSchemeType
+    value: ColorSchemeType,
+    icon: string,
 }[] = [
         {
             label: 'Light',
             value: 'light',
+            icon: 'mdi-white-balance-sunny',
         },
         {
             label: 'System',
             value: 'no-preference',
+            icon: isOnMobile.value ? 'mdi-cellphone' : 'mdi-laptop',
         },
         {
             label: 'Dark',
             value: 'dark',
+            icon: 'mdi-brightness-2',
         }
     ];
 
 </script>
 
 <template>
-    <SelectButton v-model="colorScheme" :options="options" :option-value="o => o.value" :option-label="o => o.label" />
+    <SelectButton class="flex w-full flex-row" v-model="colorScheme" :options="options" :option-value="o => o.value"
+        :pt="{ button: { class: 'w-full' } }">
+        <template #option="slotProps">
+            <div class="flex flex-row flex-nowrap gap-3">
+                <i class="mdi" :class="slotProps.option.icon" />
+                <span>{{ slotProps.option.label }}</span>
+            </div>
+        </template>
+    </SelectButton>
 </template>
