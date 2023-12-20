c5cfc86990df6aa17f7bbb4833b72674d39c5d42
dasdawidt <David_Maier@gmx.de>
2023-12-20 15:37:52 +0100

    fix: footer text justification

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="flex flex-row flex-wrap mx-auto justify-center items-center w-full opacity-25 gap-1 py-2 text-xs"
+    <div class="flex flex-row flex-wrap mx-auto justify-center items-center w-full opacity-25 gap-1 py-2 text-xs text-center"
         style="color: var(--text-color)">
         <span>
             A project by <a href="https://www.dasdawidt.com" style="color: var(--text-color)"
                 class="underline underline-offset-4 hover:opacity-50 transition-opacity ease-in-out">David Maier</a>.
         </span>
         <span>
-            This website does not collect any personal data.
+            This website does not collect any personal data and only uses technical cookies.
             <a href="https://www.dasdawidt.com/imprint" style="color: var(--text-color)" target="_blank"
                 class="underline underline-offset-4 hover:opacity-50 transition-opacity ease-in-out">Imprint</a>.
         </span>
