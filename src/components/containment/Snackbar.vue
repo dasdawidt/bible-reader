ea8664cc52a1f758428218569dac97fad93922d3
dasdawidt <David_Maier@gmx.de>
2023-12-21 00:40:07 +0100

    fix: snackbar width and layout

--- src/components/containment/Snackbar.vue
+++ src/components/containment/Snackbar.vue
@@ -1,7 +1,26 @@
 <script setup lang="ts">
-import Toast from 'primevue/toast';
+import Toast, { ToastPassThroughOptions } from 'primevue/toast';
+
+const passThrough: ToastPassThroughOptions = {
+    root: {
+        style: {
+            right: '0',
+            bottom: '0',
+            padding: '20px',
+            width: '100%',
+            maxWidth: '400px',
+            pointerEvents: 'none'
+        }
+    },
+    container: {
+        style: {
+            pointerEvents: 'auto'
+        }
+    }
+};
+
 </script>
 
 <template>
-    <Toast position="bottom-right" />
+    <Toast position="bottom-right" :pt="passThrough" />
 </template>
