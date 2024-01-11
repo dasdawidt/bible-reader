8a34fa1c7b964bbdd339d1d06336f1dc3140abec
dasdawidt <David_Maier@gmx.de>
2024-01-11 21:45:39 +0100

    feat: simplify code by using string enums

--- src/components/inputs/BookDialog.vue
+++ src/components/inputs/BookDialog.vue
@@ -32,7 +32,7 @@ const selectedBook = computed({
 
 const groupedBooks = computed(() => props.books.reduce<[{ name: string, books: Book[] }, { name: string, books: Book[] }]>(
     (obj, b) => {
-        Object.keys(BookTypeOldTestament).includes(b.type.toString())
+        Object.keys(BookTypeOldTestament).includes(b.type)
             ? obj[0].books = [...obj[0].books, b]
             : obj[1].books = [...obj[1].books, b];
         return obj;
