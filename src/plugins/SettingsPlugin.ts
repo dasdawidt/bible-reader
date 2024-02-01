71fc5cd5b2234795f32d11da66f99dab5c2fba35
dasdawidt <David_Maier@gmx.de>
2024-02-01 23:35:27 +0100

    fix: use custom implementation of use storage

--- src/plugins/SettingsPlugin.ts
+++ src/plugins/SettingsPlugin.ts
@@ -1,49 +1,82 @@
-import { ColorSchemeType, RemovableRef, useStorage } from '@vueuse/core';
-import { Plugin, watch } from 'vue';
+import { ColorSchemeType } from '@vueuse/core';
+import { Plugin, ref, watch } from 'vue';
+import I18nPlugin from './I18nPlugin';
+import { useTheme } from './ThemePlugin';
 
-const persistenceEnabled = useStorage<boolean>(
-    'settings/enabled',
-    undefined,
-    localStorage,
-    {
-        initOnMounted: true,
-        flush: 'sync',
-        serializer: {
-            read: (raw) => JSON.parse(raw),
-            write: (value) => JSON.stringify(value),
-        },
-    }
-);
-let settings: RemovableRef<Settings> = null;
+const theme = useTheme();
+const { locale } = I18nPlugin.global;
+
+const SETTINGS_KEY = 'settings/settings';
+const PERSISTENCE_KEY = 'settings/enabled';
 
 type Settings = {
     theme: ColorSchemeType;
-    language: string;
+    language: typeof locale.value;
     translation: string;
 };
 
 const defaultSettings: Settings = {
     theme: 'no-preference',
-    language: 'en-us',
+    language: 'en',
     translation: 'kjv',
 };
 
-function setup(defaultSettingsOverride = defaultSettings) {
-    settings = useStorage(
-        'settings/settings',
-        defaultSettingsOverride,
-        localStorage,
-        {
-            mergeDefaults: true,
-            deep: true,
-        }
-    );
+const persistenceEnabled = ref<boolean>(false);
+const settings = ref<Settings>(defaultSettings);
+
+watch(
+    settings,
+    (newValue, _oldValue) => {
+        theme.value = newValue.theme;
+        locale.value = newValue.language;
+    },
+    {
+        deep: true,
+    }
+);
+
+function read(key: string) {
+    try {
+        return JSON.parse(localStorage.getItem(key));
+    } catch (e) {
+        console.error(e);
+        return undefined;
+    }
+}
+
+function write(key: string, value: any) {
+    localStorage.setItem(key, JSON.stringify(value));
+}
+
+function remove(key: string) {
+    localStorage.removeItem(key);
 }
 
 const SettingsPlugin: Plugin = {
     install(_app, ..._options) {
-        watch(persistenceEnabled, (_oldValue, newValue) =>
-            newValue ? setup() : undefined
+        persistenceEnabled.value = read(PERSISTENCE_KEY) ?? false;
+        if (persistenceEnabled.value) {
+            settings.value = read(SETTINGS_KEY) ?? defaultSettings;
+        }
+        watch(persistenceEnabled, (value, _oldValue) => {
+            if (value) {
+                write(PERSISTENCE_KEY, persistenceEnabled.value);
+                write(SETTINGS_KEY, settings.value);
+            } else {
+                remove(PERSISTENCE_KEY);
+                remove(SETTINGS_KEY);
+            }
+        });
+        watch(
+            settings,
+            (value, _oldValue) => {
+                if (persistenceEnabled.value) {
+                    write(SETTINGS_KEY, value);
+                }
+            },
+            {
+                deep: true,
+            }
         );
     },
 };
