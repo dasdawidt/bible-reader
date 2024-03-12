4ed5f4c3fac931fba87e1e8f4952b59ea3e3fadc
dasdawidt <David_Maier@gmx.de>
2024-03-12 10:43:50 +0100

    feat: add debounce and reset to search

--- src/components/inputs/SearchDialog.vue
+++ src/components/inputs/SearchDialog.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
-import { useOnMobile } from "@/logic/util/MobileDetection";
-import { useFocus } from "@vueuse/core";
-import Button from "primevue/button";
-import Dialog from "primevue/dialog";
+import { useOnMobile } from '@/logic/util/MobileDetection';
+import { refDebounced, useFocus } from '@vueuse/core';
+import Button from 'primevue/button';
+import Dialog from 'primevue/dialog';
 import IconField from 'primevue/iconfield';
 import InputIcon from 'primevue/inputicon';
-import InputText from "primevue/inputtext";
-import { ref } from "vue";
+import InputText from 'primevue/inputtext';
+import { ref } from 'vue';
 
 const props = defineProps();
 
@@ -17,27 +17,60 @@ const visible = ref(false);
 const searchBar = ref<HTMLElement>();
 const { focused } = useFocus(searchBar);
 const search = ref('');
+const searchDebounced = refDebounced(search, 500);
+
+function reset() {
+    search.value = '';
+}
 </script>
 
 <template>
-    <Button icon="mdi-magnify" icon-class="mdi scale-150" :label="$t('prompts.search')" severity="secondary" text
-        @click="visible = true" @keyup.enter="visible = true" v-bind="$attrs" />
-    <Dialog v-model:visible="visible" :closable="false" :draggable="false" modal dismissable-mask
-        :position="isOnMobile ? 'bottom' : 'top'" class="w-full max-w-container"
-        :pt="{ content: { class: 'overflow-hidden' } }" @show="focused = true">
+    <Button
+        icon="mdi-magnify"
+        icon-class="mdi scale-150"
+        :label="$t('prompts.search')"
+        severity="secondary"
+        text
+        @click="visible = true"
+        @keyup.enter="visible = true"
+        v-bind="$attrs"
+    />
+    <Dialog
+        v-model:visible="visible"
+        :closable="false"
+        :draggable="false"
+        modal
+        dismissable-mask
+        :position="isOnMobile ? 'bottom' : 'top'"
+        class="w-full max-w-container"
+        :pt="{ content: { class: 'overflow-hidden' } }"
+        @show="focused = true"
+        @hide="reset"
+    >
         <template #header>
             <IconField iconPosition="left" class="w-full">
                 <InputIcon>
                     <i class="mdi mdi-magnify scale-150 px-1" />
                 </InputIcon>
-                <InputText class="w-full" v-model="search" :placeholder="$t('prompts.search')" ref="searchBar" />
+                <InputText
+                    class="w-full"
+                    v-model="search"
+                    :placeholder="$t('prompts.search')"
+                    ref="searchBar"
+                />
             </IconField>
         </template>
         <template #default>
-            {{ search }}
+            {{ searchDebounced }}
         </template>
         <template #footer>
-            <Button :label="$t('prompts.close')" @click="visible = false" severity="secondary" text class="w-full" />
+            <Button
+                :label="$t('prompts.close')"
+                @click="visible = false"
+                severity="secondary"
+                text
+                class="w-full"
+            />
         </template>
     </Dialog>
 </template>
