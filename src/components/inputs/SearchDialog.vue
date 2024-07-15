c396453274fefbdbc064dfeabb56f15bc4e94daf
dasdawidt <David_Maier@gmx.de>
2024-07-15 23:58:44 +0200

    feat: basic and poorly performing search functionality

--- src/components/inputs/SearchDialog.vue
+++ src/components/inputs/SearchDialog.vue
@@ -1,14 +1,20 @@
 <script setup lang="ts">
+import { useIndexList } from '@/logic/indexes';
 import { useOnMobile } from '@/logic/util/MobileDetection';
+import { IndexedVerse } from '@/types/index/indexedVerse';
+import SvgIcon from '@jamescoyle/vue-icon';
 import { mdiMagnify } from '@mdi/js';
 import { refDebounced, useFocus } from '@vueuse/core';
+import Fuse, { FuseResult } from 'fuse.js';
 import Button from 'primevue/button';
 import Dialog from 'primevue/dialog';
+import Dropdown from 'primevue/dropdown';
 import IconField from 'primevue/iconfield';
+import InputGroup from 'primevue/inputgroup';
 import InputIcon from 'primevue/inputicon';
 import InputText from 'primevue/inputtext';
-import SvgIcon from '@jamescoyle/vue-icon';
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import { useRouter } from 'vue-router';
 
 const props = defineProps();
 
@@ -24,6 +30,42 @@ const searchDebounced = refDebounced(search, 500);
 function reset() {
     search.value = '';
 }
+
+const { loading, indexList } = useIndexList();
+const results = ref<FuseResult<IndexedVerse>[]>([]);
+const selectedTranslationId = ref<string>();
+let stopHandle;
+watch(loading, async (to, from) => {
+    watch(selectedTranslationId, async (v) => {
+        if (stopHandle != null) stopHandle();
+        const fuse = new Fuse(
+            indexList.value.find(
+                (t) => t[0].translation.id === selectedTranslationId.value
+            ),
+            {
+                keys: ['verse.text'],
+            }
+        );
+        stopHandle = watch(
+            () => searchDebounced.value,
+            async (v) => (results.value = fuse.search(v, { limit: 5 }))
+        );
+    });
+});
+
+const router = useRouter();
+async function goToVerse(verse: IndexedVerse) {
+    await router.push({
+        name: 'reader',
+        query: {
+            t: verse.translation.id.toLowerCase(),
+            b: verse.book.type.toLowerCase(),
+            c: verse.chapter.number,
+            v: verse.verse.number,
+        },
+    });
+    visible.value = false;
+}
 </script>
 
 <template>
@@ -34,6 +76,7 @@ function reset() {
         @click="visible = true"
         @keyup.enter="visible = true"
         severity="secondary"
+        :loading
         text
         :pt="{
             root: { class: 'flex flex-row gap-2.5' },
@@ -62,25 +105,53 @@ function reset() {
         @hide="reset"
     >
         <template #header>
-            <IconField iconPosition="left" class="w-full">
-                <InputIcon>
-                    <SvgIcon
-                        class="!scale-150 mx-0.5"
-                        type="mdi"
-                        size="16"
-                        :path="mdiMagnify"
+            <InputGroup>
+                <IconField iconPosition="left" class="w-full">
+                    <InputIcon>
+                        <SvgIcon
+                            class="!scale-150 mx-0.5"
+                            type="mdi"
+                            size="16"
+                            :path="mdiMagnify"
+                        />
+                    </InputIcon>
+                    <InputText
+                        class="w-full rounded-r-none"
+                        v-model="search"
+                        :placeholder="$t('prompts.search')"
+                        ref="searchBar"
                     />
-                </InputIcon>
-                <InputText
-                    class="w-full"
-                    v-model="search"
-                    :placeholder="$t('prompts.search')"
-                    ref="searchBar"
+                </IconField>
+                <Dropdown
+                    class="w-1/2"
+                    :options="indexList"
+                    v-model="selectedTranslationId"
+                    option-value="0.translation.id"
+                    option-label="0.translation.name"
                 />
-            </IconField>
+            </InputGroup>
         </template>
         <template #default>
-            {{ searchDebounced }}
+            <div class="flex flex-col items-stretch gap-2">
+                <Button
+                    v-for="(result, i) of results"
+                    :key="i"
+                    severity="contrast"
+                    text
+                    pt:root:class="flex flex-col items-start text-left"
+                    @click="goToVerse(result.item)"
+                >
+                    <span class="font-semibold">
+                        {{ result.item.verse.text }}
+                    </span>
+                    <span class="opacity-50">
+                        {{ result.item.book.abbreviation }}
+                        {{ result.item.chapter.number }}:{{
+                            result.item.verse.number
+                        }}
+                    </span>
+                </Button>
+            </div>
         </template>
         <template #footer>
             <Button
