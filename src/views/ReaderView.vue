77417a3164c20017f3bf8c92d62882924483ac71
dasdawidt <David_Maier@gmx.de>
2026-09-04 15:05:28 +0200

    feat: update dependencies

--- src/views/ReaderView.vue
+++ src/views/ReaderView.vue
@@ -209,9 +209,11 @@ useTitle(
                     <h1
                         class="m-0 text-3xl text-center font-bold whitespace-nowrap overflow-hidden text-ellipsis shrink-0"
                     >
-                        {{ t('bible.chapter', {}, {
+                        {{
+                            t('bible.chapter', {}, {
                                 locale: selectedTranslation?.language?.toLowerCase(),
-                            }) }}
+                            })
+                        }}
                         {{ selectedChapter?.number }}
                     </h1>
                     <Divider class="shrink" />
